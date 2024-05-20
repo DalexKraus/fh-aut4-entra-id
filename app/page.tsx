c18d098f87483d44a45a70fab92f9e552f9f2afd
DalexKraus <dalex.kraus@gmail.com>
2024-05-21 00:31:24 +0200

    Use closure instead of dedicated function for signIn and signOut

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,6 @@
 // This ensures that the functionality from NextAuth.js is clearly visible.
 import * as NEXT_AUTH from "next-auth/react";
 
-function performLogin() {
-	NEXT_AUTH.signIn("azure-ad");
-}
-
-function performLogout() {
-	NEXT_AUTH.signOut();
-}
-
 function ProfileComponent() {
 	const { data: session, status } = NEXT_AUTH.useSession();
 
@@ -59,14 +51,14 @@ function ActionComponent() {
 		<div className="max-w-7xl mx-auto px-4 py-16 sm:px-6 lg:px-8">
 			{status === "authenticated" ? (
 				<button
-					onClick={performLogout}
+					onClick={ () => NEXT_AUTH.signOut() }
 					className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
 				>
 					Log out
 				</button>
 			) : (
 				<button
-					onClick={performLogin}
+					onClick={ () => NEXT_AUTH.signIn("azure-ad") }
 					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 				>
 					Log in with Entra ID
